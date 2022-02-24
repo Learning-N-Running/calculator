5862cc8fd55aef71d4285763b7eb5e20daf71639
jmwoo <dnwlals00@gmail.com>
2022-02-25 02:07:58 +0900

    make detailed groupInformation

--- dump_script.sql
+++ dump_script.sql
@@ -1,4 +1,9 @@
 BEGIN TRANSACTION;
+CREATE TABLE Participation(groupId INTEGER, 
+                            id char(15), 
+                            PRIMARY KEY(groupId, id),
+                            FOREIGN KEY(groupId) REFERENCES'UserGroup'('groupId'), 
+                            FOREIGN KEY(id) REFERENCES'UserTable'('id'));
 CREATE TABLE UserGroup(groupId INTEGER PRIMARY KEY, groupName VARCHAR(30) unique, groupPw VARCHAR(15));
 INSERT INTO "UserGroup" VALUES(1,'a','123');
 INSERT INTO "UserGroup" VALUES(2,'b','123');
@@ -8,4 +13,10 @@ INSERT INTO "UserTable" VALUES('jh_id','jh_name','seungeun020309@gmail.com','jh_
 INSERT INTO "UserTable" VALUES('mk_id','mk_name','seungeun020309@gmail.com','mk_pw');
 INSERT INTO "UserTable" VALUES('ddur_id','ddur_name','seungeun020309@gmail.com','ddur_pw');
 INSERT INTO "UserTable" VALUES('id0221','id0221','dnwlals00@gmail.com','aa');
+INSERT INTO "UserTable" VALUES('id1','11','aa@gmail.com','aa');
+INSERT INTO "UserTable" VALUES('id2','22','bb@gmail.com','bb');
+
+
+
+
 COMMIT;
