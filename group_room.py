1dec91fe3f31c8be0cc84ff52b1abcb04b65e8b8
tina <tina020309@naver.com>
2022-02-24 14:13:44 +0900

    add go_back_button in group_room

--- group_room.py
+++ group_room.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter.font import *
 import tkinter.messagebox as msgbox
 from tkinter import ttk
-from turtle import bgcolor, width
+from turtle import bgcolor, left, width
 
 def openFrame(frame):
     frame.tkraise()
@@ -19,9 +19,12 @@ class grouproom:
 
         self.font1=Font(family="맑은 고딕", size=30)
         self.font2=Font(family="맑은 고딕", size=15)
+        
 
         self.group_name_label = Label(self.window, text=self.group_name, font=self.font1)
         self.group_name_label.pack(pady=20)
+
+
 
         self.left_space_label = Label(self.window,text="")
         self.left_space_label.pack(ipadx=20,ipady=220,side="left")
@@ -46,60 +49,61 @@ class grouproom:
 
             #이건 db에서 갖고 와서 넣어줘야 함
 
-            #모임 참여자 frame
-        self.container = Frame(self.group_member_frame,width=320,height=40)
-        self.container.pack(side='left',fill='both', expand=True,padx=5)
-        self.container.configure(bg='red')
+            #모임 참여자 scrollable frame
+        self.group_member_container = Frame(self.group_member_frame,width=320,height=40)
+        self.group_member_container.pack(side='left',fill='both', expand=True,padx=5)
+        self.group_member_container.configure(bg='red')
 
-        self.canvas = Canvas(self.container,width=320,height=40)
-        self.canvas.pack(side='top',fill='both',expand=True)
-        self.canvas.configure(bg='green')
+        self.group_member_canvas = Canvas(self.group_member_container,width=320,height=40)
+        self.group_member_canvas.pack(side='top',fill='both',expand=True)
+        self.group_member_canvas.configure(bg='green')
 
 
-        self.scrollbar = ttk.Scrollbar(self.container,orient="horizontal",command=self.canvas.xview)
-        self.scrollbar.pack(side='bottom',fill='x')
-        self.scrollable_frame = ttk.Frame(self.canvas)
+        self.group_member_scrollbar = ttk.Scrollbar(self.group_member_container,orient="horizontal",command=self.group_member_canvas.xview)
+        self.group_member_scrollbar.pack(side='bottom',fill='x')
+        self.group_member_scrollable_frame = ttk.Frame(self.group_member_canvas)
 
-        self.scrollable_frame.bind(
+        self.group_member_scrollable_frame.bind(
             "<Configure>",
-            lambda e: self.canvas.configure(
-                scrollregion= self.canvas.bbox("all")
+            lambda e: self.group_member_canvas.configure(
+                scrollregion= self.group_member_canvas.bbox("all")
             )
         )
-        self.canvas.create_window((0,0),window=self.scrollable_frame,anchor='n')
-        self.canvas.configure(xscrollcommand=self.scrollbar.set)
-
+        self.group_member_canvas.create_window((0,0),window=self.group_member_scrollable_frame,anchor='n')
+        self.group_member_canvas.configure(xscrollcommand=self.group_member_scrollbar.set)
 
         for i in range(10):
-            line = Button(self.scrollable_frame, text="이름{}".format(i),width=8,height= 3)
+            line = Button(self.group_member_scrollable_frame, text="이름{}".format(i),width=8,height= 3)
             globals()['self.button{}.format(i)'] = line
             globals()['self.button{}.format(i)'].pack(side='left')
-            # globals()['self.button{}.format(i)'].configure(bg='ivory')
-
-
+        
+            #그룹 멤버 추가, 삭제 버튼
         self.group_member_delete_button = Button(self.group_member_frame,text="삭제",font=self.font2,width=4)
         self.group_member_delete_button.pack(side='right',ipadx=2,padx=2)
-        self.group_member_delete_button.configure(bg='ivory')
+        self.group_member_delete_button.configure(fg='red')
 
         self.group_member_add_button = Button(self.group_member_frame,text="추가",font=self.font2,width=4)
         self.group_member_add_button.pack(side='right',ipadx=2)
-        self.group_member_add_button.configure(bg='ivory')
+        self.group_member_add_button.configure(fg='green')
 
 
-        #이벤트 추가 프레임
+
+        #이전으로 ,이벤트 추가 프레임
         self.add_event_frame = Frame(self.window)
         self.add_event_frame.pack(side='top',ipadx=280,ipady=0) 
 
-        self.add_event_button = Button(self.add_event_frame, text='이벤트 추가',font=self.font2)
-        self.add_event_button.pack(side='left',ipadx=5)
+        self.go_back_button = Button(self.add_event_frame, text='이전으로',font=self.font2)
+        self.go_back_button.pack(side='left',ipadx=5)
 
-        #추가
+        self.add_event_button = Button(self.add_event_frame, text='이벤트 추가',font=self.font2)
+        self.add_event_button.pack(side='right',ipadx=5)
+
+            #이벤트 버튼 scrollable frame
         self.add_event_container = Frame(self.window,bd=1,relief='solid')
         self.add_event_container.pack(side='left',fill='both',expand=True,pady=5)
 
         self.add_event_canvas = Canvas(self.add_event_container) 
         self.add_event_canvas.pack(side='left',fill='both',expand=True)
-
 
         self.add_event_scrollbar = ttk.Scrollbar(self.add_event_container,orient="vertical",command=self.add_event_canvas.yview)
         self.add_event_scrollbar.pack(side='right',fill='y')
@@ -115,7 +119,6 @@ class grouproom:
 
         self.add_event_canvas.create_window((0,0),window=self.add_event_scrollable_frame,anchor='nw',width=560)
         self.add_event_canvas.configure(yscrollcommand=self.add_event_scrollbar.set)
-
 
         for i in range(10,0,-1):
             Button(self.add_event_scrollable_frame, text="Sample scrolling button{}".format(i),width=80,height= 7).pack()
