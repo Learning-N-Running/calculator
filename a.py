55f6bd17ce07c30cb4e31e1dd7891d19ca387aad
tina <tina020309@naver.com>
2022-02-26 21:36:15 +0900

    activate search in search_group but error exists

--- a.py
+++ a.py
@@ -1,4 +1,12 @@
-a =2
-if a==1:
-    print("안녕하세요")
-print("ㅋㅋ")
+all_group_list = ['얄라얄라','숭숭','얄라리','송얄라']
+searstng = str(input())
+n = len(searstng)
+sear_result_list = []
+for group in all_group_list:
+    k = len(group)
+    if k>=n:
+        for i in range(k-n+1):
+            if group[i:i+n]==searstng:
+                sear_result_list.append(group)
+                break
+print(sear_result_list)
--- search_group.py
+++ search_group.py
@@ -7,6 +7,15 @@ from tkinter.ttk import Labelframe
 from tkinter.font import *
 from UserInfoDB import get_all_groups
 
+class make_search_group_class():
+    def __init__(self,parent,sear_result):
+        self.sear_result = sear_result
+        # self.sear_result_button = Button(parent.search_group_scrollable_frame, text=self.sear_result,width=8,height= 3,command=lambda:[self.group_member_button_func(parent)]).pack()
+        self.sear_result_button = Button(parent.search_group_scrollable_frame, text=self.sear_result,width=44,height= 1,font=parent.font2,command=lambda:[self.sear_result_button_func(parent)]).pack()
+
+    def sear_result_button_func(self,parent):
+        print(self.sear_result)
+        #아래에 더 구현해보기
 
 
 
@@ -17,7 +26,7 @@ class searchgroup:
         self.newin.geometry("640x480")
         self.newin.resizable(False,False)
 
-        all_group_list= get_all_groups()
+        self.all_group_list= get_all_groups()
 
         self.font1=Font(family="맑은 고딕", size=30)
         self.font2=Font(family="맑은 고딕", size=15)
@@ -45,6 +54,7 @@ class searchgroup:
         self.search_bar_entry = Entry(self.third_space_frame,width=30,font=self.font2,textvariable=self.search_var)
         self.search_bar_entry.pack(pady=10)
 
+        self.search_var.trace('w',self.callback)
 
         self.fourth_space_frame = Frame(self.newin,height=60)
         self.fourth_space_frame.pack(fill='x',side='top')
@@ -56,29 +66,47 @@ class searchgroup:
         self.fifth_space_frame = Frame(self.newin)
         self.fifth_space_frame.pack(fill='both',side='top',expand=True)
             # 찾은 그룹들 버튼이 나타나는 scrollable
-        self.container = Frame(self.fifth_space_frame,bd=1,relief='solid')
-        self.container.pack(side='left',fill='both',expand=True)
+        self.search_group_container = Frame(self.fifth_space_frame,bd=1,relief='solid')
+        self.search_group_container.pack(side='left',fill='both',expand=True)
 
-        self.canvas = Canvas(self.container) 
-        self.canvas.pack(side='left',fill='both',expand=True)
+        self.search_group_canvas = Canvas(self.search_group_container) 
+        self.search_group_canvas.pack(side='left',fill='both',expand=True)
 
-        self.scrollbar = ttk.Scrollbar(self.container,orient="vertical",command=self.canvas.yview)
-        self.scrollbar.pack(side='right',fill='y')
-        self.scrollable_frame = ttk.Frame(self.canvas)
-        self.scrollable_frame.bind(
+        self.search_group_scrollbar = ttk.Scrollbar(self.search_group_container,orient="vertical",command=self.search_group_canvas.yview)
+        self.search_group_scrollbar.pack(side='right',fill='y')
+        self.search_group_scrollable_frame = ttk.Frame(self.search_group_canvas)
+        self.search_group_scrollable_frame.bind(
             "<Configure>",
-            lambda e: self.canvas.configure(
-                scrollregion= self.canvas.bbox("all")
+            lambda e: self.search_group_canvas.configure(
+                scrollregion= self.search_group_canvas.bbox("all")
             )
         )
         # self.scrollable_frame.configure(bg='red')
 
-        self.canvas.create_window((0,0),window=self.scrollable_frame,anchor='nw')
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.search_group_canvas.create_window((0,0),window=self.search_group_scrollable_frame,anchor='nw')
+        self.search_group_canvas.configure(yscrollcommand=self.search_group_scrollbar.set)
 
 
-        for i in range(7,0,-1):
-            Button(self.scrollable_frame, text="Sample scrolling button{}".format(i),width=44,height= 1,font=self.font2).pack()
+        # for i in ['A','B','C']:
+        #     # Button(self.search_group_scrollable_frame, text="Sample scrolling button{}".format(i),width=44,height= 1,font=self.font2).pack()
+        #     globals()['self.{}_search_group_class'.format(i)] = make_search_group_class(self,i)
+
+    def callback(self,*args):
+        searstng = self.search_var.get()
+        n = len(searstng)
+        sear_result_list = []
+        for group in self.all_group_list:
+            k = len(group)
+            if k>=n:
+                for i in range(k-n+1):
+                    if group[i:i+n]==searstng:
+                        sear_result_list.append(group)
+                        break
+        for sear_result in sear_result_list:
+            globals()['self.{}_search_group_class'.format(sear_result)] = make_search_group_class(self,sear_result)
+
+
+        
 
 
 
